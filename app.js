e2c348a0af2173e7fac970cb6265c42f605c36f6
Yaseen <notsolvablehat@gmail.com>
2025-08-19 09:49:21 +0530

    include from cors

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ dbConnect();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://healthcare-frontend-lovat.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser())
 app.use("/user", userRoutes);
